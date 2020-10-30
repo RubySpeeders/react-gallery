edc7e10e78fdae3dab2013da37cda634dfa272d8
Sarah Peters <sarahnpeters@gmail.com>
2020-10-30 17:20:37 -0500

    axios get route and adding a .map in the render of App.js to show gallery items on the webpate. Looks like a turd

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    galleryList: [],
+    errMsg: null,
+  };
+
+  componentDidMount() {
+    this.getPhotos();
+  }
+
+  getPhotos() {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        this.setState({
+          galleryList: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          errMsg: 'Could not get gallery list',
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,8 +37,22 @@ class App extends Component {
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br />
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg" alt="small goat" />
+        <p>My Gallery</p>
+        <div>
+          {this.state.galleryList.map((item) => {
+            return (
+              <ul key={item.id}>
+                <dt>
+                  <img src={item.path} alt={item.description} />
+                </dt>
+                <dt>{item.description}</dt>
+                <button>LIKE</button>
+                <span> {item.likes} likes</span>
+              </ul>
+            );
+          })}
+        </div>
+        {/* <img src="images/goat_small.jpg" alt="small goat" /> */}
       </div>
     );
   }
