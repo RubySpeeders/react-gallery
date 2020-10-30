2a05e7e85fda956dcb8534086a380982a051098b
Sarah Peters <sarahnpeters@gmail.com>
2020-10-30 16:25:20 -0500

    adding to gallery items to gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,34 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: 'images/goat_small.jpg',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: 'images/winter_runner.jpg',
+    description: 'Man running through winter snow.',
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: 'images/fire_city.jpg',
+    description: 'City vs country. Wildfires near the city in California.',
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: 'images/kiwis.jpg',
+    description: 'Sliced kiwis and walnuts.',
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: 'images/lotus_flower.jpg',
+    description: 'Lotus flower, like MY Loudis flower!',
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
