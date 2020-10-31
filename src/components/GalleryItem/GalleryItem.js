14b6096258c38d1125ab270f221e863687cfcc5a
Sarah Peters <sarahnpeters@gmail.com>
2020-10-31 17:57:03 -0500

    adding togglePic to toggle between pic and description

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+  state = {
+    isClicked: false,
+  };
+
   handleLikes = (event) => {
-    this.props.callBack(this.props.item.id);
+    this.props.likeCallBack(this.props.item.id);
+  };
+
+  togglePic = (event) => {
+    if (this.state.isClicked === false) {
+      this.setState({
+        isClicked: true,
+      });
+    } else {
+      this.setState({
+        isClicked: false,
+      });
+    }
   };
 
   render() {
     //variable for property coming over from GalleryList
     const prop = this.props.item;
+    let galleryItem = <img src={prop.path} alt={prop.description} />;
+    if (this.state.isClicked === true) {
+      galleryItem = <p>{prop.description}</p>;
+    }
     return (
       <div key={prop.id}>
-        <img src={prop.path} alt={prop.description} />
-        <p>{prop.description}</p>
+        <div className="whatever">
+          <div onClick={this.togglePic}>{galleryItem}</div>
+        </div>
+        {/* <img src={prop.path} alt={prop.description} onClick={this.togglePic} />
+        <p>{prop.description}</p> */}
         <button onClick={this.handleLikes}>LIKE</button>
         <span> {prop.likes} likes</span>
       </div>
