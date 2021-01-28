ae8a6f0b273ceaaae1603930d120ff065b03878f
Sarah Peters <sarahnpeters@gmail.com>
2021-01-27 21:02:58 -0600

    post route to add image to gallery server side

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -2,25 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-// PUT Route
+// PUT Route to update likes
 router.put('/like/:id', (req, res) => {
   //update number of likes on a photo
   const galleryId = req.params.id;
-  // for (const galleryItem of galleryItems) {
-  //   if (galleryItem.id == galleryId) {
-  //     galleryItem.likes += 1;
-  //   }
-  // }
   const queryText = `SELECT id, likes from "gallery" WHERE id = $1;`;
   const queryArray = [galleryId];
   pool
     .query(queryText, queryArray)
     .then((dbResponse) => {
-      const id = dbResponse.rows[0].id;
+      // const id = dbResponse.rows[0].id;
       let likes = dbResponse.rows[0].likes;
       let newLikes = likes + 1;
       const queryText = `UPDATE "gallery" SET "likes" = $1 WHERE "id" = $2;`;
-      const queryArray = [newLikes, id];
+      const queryArray = [newLikes, galleryId];
       pool
         .query(queryText, queryArray)
         .then((dbResponse) => {
@@ -36,11 +31,6 @@ router.put('/like/:id', (req, res) => {
     });
 }); // END PUT Route
 
-// // GET Route
-// router.get('/', (req, res) => {
-//   res.send(galleryItems);
-// }); // END GET Route
-
 //GET ROUTE to get all items from gallery
 router.get('/', (req, res) => {
   const queryText = `SELECT * FROM "gallery";`;
@@ -51,6 +41,24 @@ router.get('/', (req, res) => {
     })
     .catch((err) => {
       console.log('error getting gallery items', err);
+      res.sendStatus(500);
+    });
+});
+
+//POST ROUTE to add an item to the gallery
+router.post('/', (req, res) => {
+  const path = req.body.path;
+  const description = req.body.description;
+  const queryText = `INSERT INTO "gallery" ("path", "description", "likes") VALUES
+  ($1, $2, 0);`;
+  const queryArray = [path, description];
+  pool
+    .query(queryText, queryArray)
+    .then((dbResponse) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('error adding item to the gallery', err);
       res.sendStatus(500);
     });
 });
